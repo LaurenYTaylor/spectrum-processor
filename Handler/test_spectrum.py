a621021fe00c86d028b5a09fbbf8deecb8f6f149
GetDatCodekour <42021513+GetDatCodekour@users.noreply.github.com>
2018-12-06 10:31:45 +1100

    Update test_spectrum.py

--- Handler/test_spectrum.py
+++ Handler/test_spectrum.py
@@ -14,10 +14,25 @@ def test_read_lambda():
   lambda=Spectrum("spectrum_file")
   assert len(lambda.hdu.list)>4 "The D stands for data"
   
-  
-  
+#Now we test if specific values correlate with what the data is
 
-def test_spectrumone():
+def test_flux_one():
   #Tests to see if flux at [...] is True for x=....
-  spec1=spectrum(...)
-  assert
+  flux1=spectrum(#insert flux row number here)
+  assert flux1 == #insert value at flux row, "Data lines up with input"
+    
+#Better to be safe than sorry
+def test_flux_two():
+    flux2=spectrum(#insert flux row number here)
+    assert flux2 == #insert value at row number, "Data lines up with input"
+      
+def test_lambda_one():
+      lambda1=spectrum(#insert loglam row number here)
+      assert lambda1 == #insert value at row number, "Data lines up with input"
+        
+def test_lambda_two():
+      lambda2=spectrum(#insert loglam row number here)
+      assert lambda2 == #insert value at row number, "Data lines up with input"
+       
+   
+    
