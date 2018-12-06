fba4bb3cb28dea73e02604c200d2986d5d09834d
GetDatCodekour <42021513+GetDatCodekour@users.noreply.github.com>
2018-12-06 11:07:39 +1100

    Update test_spectrum.py

--- Handler/test_spectrum.py
+++ Handler/test_spectrum.py
@@ -1,38 +1,38 @@
 """Unit testing to confirm that our data is correct"""
 
-import math 
-from spectrum import spectrum
+import numpy as np
+from ..Spectrum import spectrum
 
 #First let's check to see if there exist data in the spectrum file
-spectrum_file="sdss.spec-xxxx-xxxxx-xxxx.fits"
+spectrum_file="spec-3586-55181-0042.fits"
 
-def test_read_flux():
-  flux=Spectrum("spectrum_file")
-  assert len(flux.hdu.list)>4, "I spy with my little eye something beginning with D"
+def spectrum_read():
+  spec=Spectrum(spectrum_file)
+  assert len(spec.hdu.list)==3, "I spy with my little eye something beginning with D"
   
-def test_read_lambda():
-  lambda=Spectrum("spectrum_file")
-  assert len(lambda.hdu.list)>4 "The D stands for data"
   
-#Now we test if specific values correlate with what the data is
+#Now we test the properties
 
+#Flux testing
 def test_flux_one():
-  #Tests to see if flux at [...] is True for x=....
-  flux1=spectrum(#insert flux row number here)
-  assert flux1 == #insert value at flux row, "Data lines up with input"
-    
+  #Tests to see if flux at [...] is True for x=....16.0137
+  flux1=flux.spec[0]
+  np.testing.assert_approx_equal(s.flux,16.0137)
+  
 #Better to be safe than sorry
 def test_flux_two():
-    flux2=spectrum(#insert flux row number here)
-    assert flux2 == #insert value at row number, "Data lines up with input"
-      
+    flux2=flux.spec[1]
+    np.testing.assert_approx_equal(s.flux,xxx)
+
+ #Lambda testing
 def test_lambda_one():
-      lambda1=spectrum(#insert loglam row number here)
-      assert lambda1 == #insert value at row number, "Data lines up with input"
+      lambda1=loglam.spec[0]
+      np.testing.assert_approx_equal(s.loglam,xxx)
         
 def test_lambda_two():
-      lambda2=spectrum(#insert loglam row number here)
-      assert lambda2 == #insert value at row number, "Data lines up with input"
-       
+      lambda2=loglam.spec[1]
+      np.testing.assert_approx_equal(s.loglam,xxx)
+      
+
    
     
