89d056af69e7274412828bff159a4696bcaf206f
GetDatCodekour <42021513+GetDatCodekour@users.noreply.github.com>
2018-12-06 11:27:38 +1100

    Update test_spectrum.py

--- Handler/test_spectrum.py
+++ Handler/test_spectrum.py
@@ -4,7 +4,7 @@ import numpy as np
 from ..Spectrum import spectrum
 
 #First let's check to see if there exist data in the spectrum file
-spectrum_file="spec-3586-55181-0042.fits"
+spectrum_file=fits.open("spec-3586-55181-0042.fits")
 
 def spectrum_read():
   spec=Spectrum(spectrum_file)
@@ -13,18 +13,17 @@ def spectrum_read():
   
 #Now we test the properties
 
-#Flux testing
+#Flux testing- Determines if we can read flux
 def test_flux_one():
-  #Tests to see if flux at [...] is True for x=....16.0137
   flux1=spec.flux[0]
-  np.testing.assert_approx_equal(flux1=6.0137)
+  np.testing.assert_approx_equal(flux1=16.0137)
   
 #Better to be safe than sorry
 def test_flux_two():
     flux2=spec.flux[1]
     np.testing.assert_approx_equal(flux2,xxx)
 
-#Lambda testing
+#Lambda testing-determines if we can read wavelength
 def test_lambda_one():
       lambda1=spec.loglam[0]
       np.testing.assert_approx_equal(lambda1,xxx)
@@ -33,7 +32,7 @@ def test_lambda_two():
       lambda2=spec.loglam[1]
       np.testing.assert_approx_equal(lambda2,xxx)
       
-#Ra test   
+#Ra test-Pretty sure  you get the point
 def test_ra_one():
       ra1=spec.ra[0]
       np.testing.assert_approx_equal(ra1,xxx)
@@ -41,4 +40,15 @@ def test_ra_one():
 def test_ra_two():
       ra2=spec.ra[1]
       np.testing.assert_approx_equal(ra2,xxx)
+      
+#Test dec
+def test_dec_one():
+      dec1=spec.dec[0]
+      np.testing.assert_approx_equal(dec1,xxx)
+      
+def test_dec_two():
+      dec2=spec.dec[1]
+      np.testing.assert_approx_equal(dec2,xxx)
+      
+ 
     
