655fffab27c97d6fcc4cd98fdfae39539f2b2f76
Lauren Taylor <lauren.y.taylor96@gmail.com>
2018-12-06 11:37:25 +1100

    Added a basic example of retrieving a value

--- Handler/spectrum.py
+++ Handler/spectrum.py
@@ -4,38 +4,51 @@ import os
 from matplotlib import pyplot as plt
 import numpy as np
 
-hdu_info = fits.open("spec-10000-57346-0007.fits")
-t1 = Table.read("spec-10000-57346-0007.fits", hdu=1)
-t2 = Table.read("spec-10000-57346-0007.fits", hdu=2)
+# hdu_info = fits.open("spec-10000-57346-0007.fits")
+# t1 = Table.read("spec-10000-57346-0007.fits", hdu=1)
+# t2 = Table.read("spec-10000-57346-0007.fits", hdu=2)
 
-print(t2['PLUG_RA'].data)
-print(t2['PLUG_DEC'].data)
-print(t2['OBJTYPE'].data)
-print(t2['CLASS'].data)
-print(t2['SUBCLASS'].data)
+# print(t2['PLUG_RA'].data)
+# print(t2['PLUG_DEC'].data)
+# print(t2['OBJTYPE'].data)
+# print(t2['CLASS'].data)
+# print(t2['SUBCLASS'].data)
 
 
 class Spectrum(object):
 	def __init__(self, filepath):
-		filepath = filepath
-		self.flux = None
-		self.loglam = None
-		self.ra = None
-		self.dec = None
-		self.targeted = None
-		self.obs_date = None
-		self.z = None
-		self.distance = None
-		self.max_flux = None
-		self.obj_class = None
-		self.obj_subclass = None
+		self.filepath = filepath
+		#self.targeted = None
+		#self.obs_date = None
+		#self.z = None
+		#self.distance = None
+		#self.max_flux = None
+		#self.obj_class = None
+		#self.obj_subclass = None
 	
-	@getter
+	@property
 	def flux(self):
 		t = Table.read(self.filepath, hdu=1)
 		flux = t['flux'].data
 		return flux
+			
+	@property
+	def loglam(self):
+		t = Table.read(self.filepath, hdu=1)
+		loglam = t['loglam'].data
+		return loglam	
 	
+	@property
+	def ra(self):
+		t = Table.read(self.filepath, hdu=2)
+		ra = t['PLUG_RA'].data[0]
+		return ra
+	
+	@property
+	def dec(self):
+		t = Table.read(self.filepath, hdu=2)
+		dec = t['PLUG_DEC'].data[0]
+		return dec
 	
 	def display_headers(self, header_num):
 		t = Table.read(self.filepath, hdu=header_num)
@@ -46,4 +59,6 @@ class Spectrum(object):
 		print(hdu.info())
 		
 	
-		
+spec = Spectrum("spec-10000-57346-0007.fits")
+print(spec.ra)
+	
