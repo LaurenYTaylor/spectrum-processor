0f001283bae1c393083c2c84e316fa19c316155b
GetDatCodekour <42021513+GetDatCodekour@users.noreply.github.com>
2018-12-06 14:58:54 +1100

    Update test_spectrum.py

--- Handler/test_spectrum.py
+++ Handler/test_spectrum.py
@@ -1,6 +1,7 @@
 """Unit testing to confirm that our data is correct"""
 
 import numpy as np
+from astropy.io import fits
 from Spectrum import spectrum
 
 #First let's check to see if there exist data in the spectrum file
@@ -21,16 +22,16 @@ def test_flux_one():
 #Better to be safe than sorry
 def test_flux_two():
     flux2=spec.flux[1]
-    np.testing.assert_approx_equal(flux2,xxx)
+    np.testing.assert_approx_equal(flux2=xxx)
 
 #Lambda testing-determines if we can read wavelength
 def test_lambda_one():
       lambda1=spec.loglam[0]
-      np.testing.assert_approx_equal(lambda1,xxx)
+      np.testing.assert_approx_equal(lambda1=xxx)
         
 def test_lambda_two():
       lambda2=spec.loglam[1]
-      np.testing.assert_approx_equal(lambda2,xxx)
+      np.testing.assert_approx_equal(lambda2=xxx)
       
 #Ra test-Pretty sure  you get the point
 def test_ra_one():
@@ -39,16 +40,16 @@ def test_ra_one():
       
 def test_ra_two():
       ra2=spec.ra[1]
-      np.testing.assert_approx_equal(ra2,xxx)
+      np.testing.assert_approx_equal(ra2=xxx)
       
 #Test dec
 def test_dec_one():
       dec1=spec.dec[0]
-      np.testing.assert_approx_equal(dec1,xxx)
+      np.testing.assert_approx_equal(dec1=xxx)
       
 def test_dec_two():
       dec2=spec.dec[1]
-      np.testing.assert_approx_equal(dec2,xxx)
+      np.testing.assert_approx_equal(dec2=xxx)
       
  
     
